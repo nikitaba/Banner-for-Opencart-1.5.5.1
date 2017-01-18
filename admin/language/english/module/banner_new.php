42b692eb80ae8c8a005fbaee25299e82074343d0
nikitaba <mykytabaranov@gmail.com>
2017-01-18 10:44:19 +0200

    v 1.0.0

---
+++ admin/language/english/module/banner_new.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']       = 'New banner:';
+
+// Text
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified module banner!';
+$_['text_content_top']    = 'Content Top';
+$_['text_content_bottom'] = 'Content Bottom';
+$_['text_column_left']    = 'Column Left';
+$_['text_column_right']   = 'Column Right';
+$_['text_image_manager']  = 'Image manager';
+$_['text_browse']         = 'Browse';
+$_['text_clear']          = 'Clear';
+
+// Entry
+$_['entry_title']         = 'Title:';
+$_['entry_description']   = 'Description:';
+$_['entry_image']         = 'Image:';
+$_['entry_dimension']     = 'Dimension (W x H):';
+$_['entry_category']      = 'Category:<br /><span class="help">(Autocomplete)</span>';
+$_['entry_layout']        = 'Layout:';
+$_['entry_position']      = 'Position:';
+$_['entry_status']        = 'Status:';
+$_['entry_sort_order']    = 'Sort order:';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module banner!';
+$_['error_dimension']     = 'Width &amp; Height dimensions required!';
---
+++ admin/language/russian/module/banner_new.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']       = 'Новый баннер';
+
+// Text
+$_['text_module']         = 'Модули';
+$_['text_success']        = 'Настройки модуля обновлены!';
+$_['text_content_top']    = 'Верх страницы';
+$_['text_content_bottom'] = 'Низ страницы';
+$_['text_column_left']    = 'Левая колонка';
+$_['text_column_right']   = 'Правая колонка';
+$_['text_image_manager']  = 'Менеджер изображений';
+$_['text_browse']         = 'Обзор';
+$_['text_clear']          = 'Очистить';
+
+// Entry
+$_['entry_title']         = 'Название:';
+$_['entry_description']   = 'Описание:';
+$_['entry_image']         = 'Изображение:';
+$_['entry_dimension']     = 'Размеры (Ш x В):';
+$_['entry_category']      = 'Категория:<br /><span class="help">(Автодополнение)</span>';
+$_['entry_layout']        = 'Макет:';
+$_['entry_position']      = 'Расположение:';
+$_['entry_status']        = 'Статус:';
+$_['entry_sort_order']    = 'Порядок сортировки:';
+
+// Error
+$_['error_permission']    = 'У Вас нет прав для управления этим модулем!';
+$_['error_dimension']     = 'Введите размеры баннера!';
